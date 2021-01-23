62360dc9fedb61fc4c696969fa8d960ec1a581fd
one02free <one02free@qq.com>
2021-01-23 12:04:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld.git' feeds.conf.default
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git /package/lean/luci-app-jd-dailybonus
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git /package/lean/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
