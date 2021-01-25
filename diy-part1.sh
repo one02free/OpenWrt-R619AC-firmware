e1a2d775f3898975b1039c16a2ce488c0af4ea0f
one02free <one02free@qq.com>
2021-01-25 16:15:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,8 @@
 
 # Uncomment a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld.git' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld.git' feeds.conf.default
 
 # Add a feed source
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
