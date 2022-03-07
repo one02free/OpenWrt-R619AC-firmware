2a5423fcc4f8b318ff1a1550973ba7121415e0e4
one02free <one02free@qq.com>
2022-03-07 10:21:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-#sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld.git' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 #sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 
